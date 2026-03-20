f93079eb81cb9600c57d9fe47ab33359cf1a6830
emaherIP <104846033+emaherIP@users.noreply.github.com>
2026-03-20 09:48:50 -0400

    Added Back to Lights

--- Github Share Example/Assets/TrafficLightController.cs
+++ Github Share Example/Assets/TrafficLightController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TrafficLightController : MonoBehaviour
 {
 
+    public GameObject testObject;
+
     public GameObject redLight, yellowLight, greenLight;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,15 @@ public class TrafficLightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawner();
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(SpawnerCoroutine());
+        }
     }
 
     IEnumerator TrafficLight()
@@ -24,13 +34,24 @@ public class TrafficLightController : MonoBehaviour
         {
             greenLight.SetActive(false);
             redLight.SetActive(true);
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(10);
             redLight.SetActive(false);
             yellowLight.SetActive(true);
             yield return new WaitForSeconds(3);
             yellowLight.SetActive(false);
             greenLight.SetActive(true);
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(30);
         }
+    }
+
+    void Spawner()
+    {
+        Instantiate(testObject);
+    }
+
+    IEnumerator SpawnerCoroutine()
+    {
+        yield return new WaitForSeconds(5f);
+        Instantiate(testObject);
     }
 }
